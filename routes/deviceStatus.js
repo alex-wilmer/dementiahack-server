34d4d87321a117c9cf984d38cca655837d14e34b
Alex Wilmer <alex.wilmer@oicr.on.ca>
2017-03-04 19:22:40 -0500

    improved device status endpoint for alex because it's his birthday <3

--- routes/deviceStatus.js
+++ routes/deviceStatus.js
@@ -1,13 +1,20 @@
 
-export default ({ router, deviceInterface }) => {
+export default ({ router, dataStore, deviceInterface }) => {
   router.get(`/deviceStatus/:deviceId`, async ctx => {
     let { deviceId } = ctx.params
     if (!deviceId) {
       ctx.body = { success: false, error: `missing deviceId` }
       return
     }
-    let value = deviceInterface.getDeviceValue(deviceId)
-    console.log(`/deviceStatus :: deviceId: ${deviceId}, value: ${value}`)
-    ctx.body = { value }
+    let isOn, timeOn
+    let device = dataStore.devices[deviceId]
+    let currentValue = deviceInterface.getDeviceValue(deviceId)
+    if (device) {
+      let { watcher, threshold } = device
+      isOn = currentValue >= threshold
+      timeOn = watcher && watcher.dangerStamp ? Date.now() - watcher.dangerStamp : null
+    }
+    console.log(`/deviceStatus :: deviceId: ${deviceId}, value: ${currentValue}`)
+    ctx.body = { currentValue, isOn, timeOn }
   })
 }
